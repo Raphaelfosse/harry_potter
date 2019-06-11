5555f5197acf27bec7a8e9a6b32a4020638e5318
Martial <martial.pouzet@laposte.net>
2019-06-11 12:17:38 +0200

    creation_classe_sorcier

---
+++ src/main/java/Sorcier.java
@@ -0,0 +1,55 @@
+
+public class Sorcier {
+	
+	private String nom;
+	private String prenom;
+	private Integer age;
+	private Enum civ;
+	private Enum patronus;
+	
+	
+	public Sorcier () {}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Enum getCiv() {
+		return civ;
+	}
+	public void setCiv(Enum civ) {
+		this.civ = civ;
+	}
+	public Enum getPatronus() {
+		return patronus;
+	}
+	public void setPatronus(Enum patronus) {
+		this.patronus = patronus;
+	}
+
+	@Override
+	public String toString() {
+		return "Sorcier [nom=" + nom + ", prenom=" + prenom + ", age=" + age + ", civ=" + civ + ", patronus=" + patronus
+				+ "]";
+	}
+	
+	
+	
+
+}
