1db97d7bfdc672a2176832ade7a3c458d6281d97
FOSSE <raphael.fosse91@gmail.com>
2019-06-11 13:34:42 +0200

    eleve class created

---
+++ src/main/java/Eleve.java
@@ -0,0 +1,32 @@
+
+public class Eleve extends Sorcier {
+	private List<Matieres> matiere; 
+	private Maison maison;
+	
+	public Eleve() {
+	}
+	
+	public Eleve(List<Matieres> matiere, Maison maison) {
+		this.matiere = matiere;
+		this.maison = maison;
+	}
+	public List<Matieres> getMatiere() {
+		return matiere;
+	}
+	public void setMatiere(List<Matieres> matiere) {
+		this.matiere = matiere;
+	}
+	public Maison getMaison() {
+		return maison;
+	}
+	public void setMaison(Maison maison) {
+		this.maison = maison;
+	}
+
+	@Override
+	public String toString() {
+		return "Eleve [getNom()=" + getNom() + ", getPrenom()=" + getPrenom() + ", getAge()=" + getAge() + ", getCiv()="
+				+ getCiv() + ", getPatronus()=" + getPatronus() + ", toString()=" + super.toString() + "]";
+	} 
+	
+}
