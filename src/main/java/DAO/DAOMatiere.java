eeefecf3c8543c5fb6ddd8a19005f8be9df2e75f
FOSSE <raphael.fosse91@gmail.com>
2019-06-11 15:13:34 +0200

    dao matiere done

---
+++ src/main/java/DAO/DAOMatiere.java
@@ -0,0 +1,77 @@
+package DAO;
+
+import java.util.List;
+import javax.persistence.*;
+import javax.persistence.Persistence;
+import metier.Matiere;;
+
+public class DAOMatiere implements DAO<Matiere, Integer> {
+
+	@Override
+	public void insert(Matiere object) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hp");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		em.persist(object);
+
+		em.getTransaction().commit();
+		em.close();
+		Context.getInstance().destroy();
+	}
+
+	@Override
+	public Matiere selectById(Integer id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hp");
+		EntityManager em = emf.createEntityManager();
+
+		Matiere s = em.find(Matiere.class, id);
+		em.close();
+		Context.getInstance().destroy();
+		return s;
+	}
+
+	@Override
+	public void update(Matiere object) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hp");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		em.merge(object);
+
+		em.getTransaction().commit();
+		em.close();
+		Context.getInstance().destroy();
+
+	}
+
+	@Override
+	public void delete(Matiere object) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hp");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		em.remove(object);
+
+		em.getTransaction().commit();
+		em.close();
+		Context.getInstance().destroy();
+	}
+
+	@Override
+	public List<Matiere> selectAll() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hp");
+		EntityManager em = emf.createEntityManager();
+
+		Query query = em.createQuery("from Grade");
+		List<Matiere> liste = query.getResultList();
+		em.close();
+		Context.getInstance().destroy();
+		return liste;
+	}
+
+}
